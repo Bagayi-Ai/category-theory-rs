39cae8b840d6d6f625b7cad86c878394542ae604
mwas <francismwangi152@gmail.com>
2025-07-18 18:43:22 +0300

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+mod generic_category;
+
+mod core {
+    pub mod category;
+    pub mod uuid_id;
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}

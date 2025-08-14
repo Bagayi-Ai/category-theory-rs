e6b3b7e69b26a548dd380990b02870d0fa876c16
mwas <francismwangi152@gmail.com>
2025-08-14 11:49:56 +0300

    update endo functor

--- src/core/product_endofunctor.rs
+++ src/core/product_endofunctor.rs
@@ -18,29 +18,26 @@ use crate::core::traits::arrow_trait::ArrowTrait;
 use crate::core::traits::category_trait::CategoryTrait;
 use std::rc::Rc;
 use crate::core::category::Category;
+use crate::core::discrete_category::DiscreteCategory;
 use crate::core::functor::Functor;
 use crate::core::product_object::ProductObject;
 
-pub fn apply_product<
-    Id: Identifier<Id = Id>,
-    SourCategory: CategoryTrait<Identifier=Id>,
-    FixeCategory: CategoryTrait,
->(
-    source_category: Rc<SourCategory>,
-    fixed_category: Rc<FixeCategory>,
+pub fn apply_product(
+    source_category: Rc<DiscreteCategory<String>>,
+    fixed_category: Rc<DiscreteCategory<String>>,
 ) -> Result<Rc<
     Functor<
-        Id,
-        SourCategory,
-        Category<Id, Category<Id, ProductObject<Id, SourCategory::Object, FixeCategory::Object>>>
+        String,
+        DiscreteCategory<String>,
+        DiscreteCategory<String>
     >
 >, Errors>
 where
-    <SourCategory as CategoryTrait>::Object: CategoryTrait<Identifier = Id>,
+    // <SourCategory as CategoryTrait>::Object: CategoryTrait<Identifier = String>,
 {
     // we take a product of the source category and the fixed category
     // and map the objects and morphisms of the source category to the target category
-    let mut target_category = Category::new();
+    let mut target_category = DiscreteCategory::new();
 
     let fixed_objects = fixed_category.get_all_objects()?;
 
@@ -48,16 +45,14 @@ where
 
     // first map the objects from the source category to the target category
     for source_object in source_category.get_all_objects()? {
-        let mut target_object = Rc::new(Category::new_with_id(source_object.category_id().clone()));
+        let mut target_object = Rc::new(DiscreteCategory::new_with_id(source_object.category_id().clone()));
         for fixed_object in &fixed_objects {
             // product object (_) * fixed_object
-            let product_object = Rc::new(ProductObject::new(
-                Id::generate(),
-                source_object.clone(),
-                (*fixed_object).clone()));
+            let new_value = (*source_object.clone()).category_id().to_owned()
+                + (*fixed_object).clone().category_id();
             Rc::get_mut(&mut target_object)
                 .ok_or(Errors::ObjectNotFound)?
-                .add_object(product_object.clone())?;
+                .add_object(Rc::new(new_value.into()))?;
         }
         target_category.add_object(target_object.clone())?;
 
@@ -81,7 +76,7 @@ where
     // }
 
     Ok(Rc::new(Functor::new(
-        Id::generate(),
+        String::generate(),
         source_category,
         Rc::new(target_category),
         mappings
